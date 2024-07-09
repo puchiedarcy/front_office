f8d106a966b371adcc2c73500554b208f1d61113
Zachary Layman <puchiedarcy@users.noreply.github.com>
2024-07-09 15:13:27 -0500

    Replace tab with 4 spaces.

--- frontOffice.s
+++ frontOffice.s
@@ -7,94 +7,94 @@
 
 .segment "STARTUP"
 reset:
-	sei ; Disable interrupts.
-	cld ; Disable decimal mode.
+    sei ; Disable interrupts.
+    cld ; Disable decimal mode.
 
-	ldx #%01000000
-	stx $4017 ; Disable API IRQ.
+    ldx #%01000000
+    stx $4017 ; Disable API IRQ.
 
-	ldx #$FF
-	txs ; Initialize stack pointer to grow down from $01FF.
+    ldx #$FF
+    txs ; Initialize stack pointer to grow down from $01FF.
 
-	inx ; Sets X register to 0.
-	stx $2000 ; Disable NMI.
-	stx $2001 ; Disable rendering.
-	stx $4015 ; Disable APU sound.
-	stx $4010 ; Disable DMC IRQ.
+    inx ; Sets X register to 0.
+    stx $2000 ; Disable NMI.
+    stx $2001 ; Disable rendering.
+    stx $4015 ; Disable APU sound.
+    stx $4010 ; Disable DMC IRQ.
 
-	bit $2002 ; Clear VBlank across reset.
+    bit $2002 ; Clear VBlank across reset.
 
-	: ; Wait for VBLank 1 as PPU initializes.
-		bit $2002 ; Bit 7 copied to N.
-		bpl :- ; Branch on N=0. Thus, loop until N=1 meaning VBlank happened.
+    : ; Wait for VBLank 1 as PPU initializes.
+        bit $2002 ; Bit 7 copied to N.
+        bpl :- ; Branch on N=0. Thus, loop until N=1 meaning VBlank happened.
 
-	; In the meantime...
-	; Clear all RAM to 0.
-	txa
-	: ; X=0. Loop until X overflows back to 0.
-		sta $0000, x
-		sta $0100, x
-		sta $0200, x
-		sta $0300, x
-		sta $0400, x
-		sta $0500, x
-		sta $0600, x
-		sta $0700, x
-		inx
-	bne :- ; Branch while X!=0.
-        
-	; Place all sprites offscreen at Y=255.
-	lda #255
-	ldx #0
-	:
-		sta oam, x ; Byte 0 of OAM is Y coordinate.
-		inx ; Skip byte 1, tile number.
-		inx ; Skip byte 2, attributes.
-		inx ; Skip byte 3, X coordinate.
-		inx ; Aligned to Byte 0 of next sprite.
-	bne :- ; Branch while X!=0.
+    ; In the meantime...
+    ; Clear all RAM to 0.
+    txa
+    : ; X=0. Loop until X overflows back to 0.
+        sta $0000, x
+        sta $0100, x
+        sta $0200, x
+        sta $0300, x
+        sta $0400, x
+        sta $0500, x
+        sta $0600, x
+        sta $0700, x
+        inx
+    bne :- ; Branch while X!=0.
 
-	: ; Wait for VBLank 2 as PPU initialization finalizes.
-		bit $2002
-		bpl :-
+    ; Place all sprites offscreen at Y=255.
+    lda #255
+    ldx #0
+    :
+        sta oam, x ; Byte 0 of OAM is Y coordinate.
+        inx ; Skip byte 1, tile number.
+        inx ; Skip byte 2, attributes.
+        inx ; Skip byte 3, X coordinate.
+        inx ; Aligned to Byte 0 of next sprite.
+    bne :- ; Branch while X!=0.
 
-	lda #%10000000
-	sta $2000 ; Enable NMIs.
+    : ; Wait for VBLank 2 as PPU initialization finalizes.
+        bit $2002
+        bpl :-
 
-	lda #%00001010
-	sta $2001 ; Enable rendering.
+    lda #%10000000
+    sta $2000 ; Enable NMIs.
 
-	; Set palette.
-	lda #$3F
-	sta $2006
-	lda #$00
-	sta $2006
+    lda #%00001010
+    sta $2001 ; Enable rendering.
 
-	lda #$0F
-	sta $2007
-	lda #$25
-	sta $2007
+    ; Set palette.
+    lda #$3F
+    sta $2006
+    lda #$00
+    sta $2006
+
+    lda #$0F
+    sta $2007
+    lda #$25
+    sta $2007
 
 main:
-	jmp main
+    jmp main
 
 nmi:
-	inx
-	stx $0000
+    inx
+    stx $0000
 
-	; Set scroll.
-	stx $2005 ; x-scroll
-	stx $2005 ; y-scroll
+    ; Set scroll.
+    stx $2005 ; x-scroll
+    stx $2005 ; y-scroll
 
-	lda #%10000000
-	sta $2000
+    lda #%10000000
+    sta $2000
 
-	rti
+    rti
 irq:
-	rti
+    rti
 
 oam:
-.res 256 ; Dedicated space for sprites.
+    .res 256 ; Dedicated space for sprites.
 
 .segment "VECTORS"
 .word nmi
