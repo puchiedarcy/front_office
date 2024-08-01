fff2b7c93413fd5dcabfee763b537357c6e70cb3
Zachary Layman <puchiedarcy@users.noreply.github.com>
2024-08-01 17:56:49 -0500

    Comment controller.s.

--- controller.s
+++ controller.s
@@ -15,24 +15,36 @@ controller1_this_frame: .res 1
 controller1_last_frame: .res 1
 
 .macro read_controller1
+    ; Move last frame's inputs
     lda controller1_this_frame
     sta controller1_last_frame
-    
-    ; Strobe CONTROLLER1_ADDR to latch buttons pressed
+
+    ; Strobe CONTROLLER1_ADDR to latch buttons held
     lda #1
     sta CONTROLLER1_ADDR
+
+    ; Sets up ring counter for counting 8 bits of input
     sta controller1_this_frame
     lsr a
+
+    ; Poll 0 to CONTROLLER1_ADDR to finish latching
     sta CONTROLLER1_ADDR
     :
         lda CONTROLLER1_ADDR
+
+        ; Bit 0 has button press status
+        ; Shift that status into Carry flag
         lsr a
+
+        ; Roll the Carry flag into this frame's held buttons
         rol controller1_this_frame
         bcc :-
+
+    ; Calculate new button presses
     lda controller1_last_frame
     eor #%11111111
     and controller1_this_frame
-    sta controller1 ; Only new button presses this frame
+    sta controller1
 .endmacro
 
 .macro on_press_goto btn, macro
