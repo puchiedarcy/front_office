0b1a2317898a2e6d03714ac0c01839324e68d177
Zachary Layman <puchiedarcy@users.noreply.github.com>
2024-08-08 17:55:26 -0500

    Bank refactor into money.

---
+++ test/tests/money.s
@@ -0,0 +1,20 @@
+.include "../test.inc"
+
+.include "../../lib/money/money.inc"
+.importzp money_total
+.import add_money
+
+.CODE
+.export _main
+_main:
+    jsr test_add_money
+    rts
+
+test_add_money:
+    lda #0
+    sta money_total
+
+    jsr add_money
+
+    assert money_total, #1, #1
+    rts
