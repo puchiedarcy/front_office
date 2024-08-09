ba26eee90605594e950f239857a09259fea82a96
Zachary Layman <puchiedarcy@users.noreply.github.com>
2024-08-09 14:29:15 -0500

    Map Maker: extract size per obj.

---
+++ map_maker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo -n "" > bin/front_office.chart
+
+declare -a objects
+declare -A object_segments
+
+while IFS= read -r line; do
+    if [[ ${line} =~ ([a-z_]+)\.o: ]]; then
+        _OBJ_NAME="${BASH_REMATCH[1]}"
+        objects+=("$_OBJ_NAME")
+        continue
+    fi
+
+    if [[ ${line} =~ ^[[:space:]]+([A-Z]+)[[:space:]]+Offs=.*Size=([0-9A-F]+).* ]]; then
+        object_segments["$_OBJ_NAME"]+="${BASH_REMATCH[1]}:${BASH_REMATCH[2]};"
+        continue
+    fi
+    
+    if [[ ${line} == "" ]]; then
+        break
+    fi
+done < <(tail -n +3 "$1")
+
+for i in "${objects[@]}"
+do
+    echo $i
+    echo "${object_segments[$i]}"
+done
