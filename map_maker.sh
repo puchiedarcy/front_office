768b79db3d2f28b35c14f80953f66079075af950
Zachary Layman <puchiedarcy@users.noreply.github.com>
2024-08-09 14:40:58 -0500

    Extract segment sizes.

--- map_maker.sh
+++ map_maker.sh
@@ -3,6 +3,7 @@ echo -n "" > bin/front_office.chart
 
 declare -a objects
 declare -A object_segments
+declare -A segments
 
 while IFS= read -r line; do
     if [[ ${line} =~ ([a-z_]+)\.o: ]]; then
@@ -15,8 +16,13 @@ while IFS= read -r line; do
         object_segments["$_OBJ_NAME"]+="${BASH_REMATCH[1]}:${BASH_REMATCH[2]};"
         continue
     fi
-    
-    if [[ ${line} == "" ]]; then
+
+    if [[ ${line} =~ ^([A-Z]+)[[:space:]]+[0-9A-F]+[[:space:]]+[0-9A-F]+[[:space:]]+([0-9A-F]+).* ]]; then
+        segments["${BASH_REMATCH[1]}"]="${BASH_REMATCH[2]}"
+        continue
+    fi
+
+    if [[ ${line} == "Exports list by name:" ]]; then
         break
     fi
 done < <(tail -n +3 "$1")
@@ -25,4 +31,10 @@ for i in "${objects[@]}"
 do
     echo $i
     echo "${object_segments[$i]}"
+done
+
+for i in "${!segments[@]}"
+do
+    echo $i
+    echo "${segments[$i]}"
 done
