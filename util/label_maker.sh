9cf7b533688dddb018bdd5f638ae414e5418db15
Zachary Layman <puchiedarcy@users.noreply.github.com>
2024-08-13 15:14:00 -0500

    Make utils.

--- util/label_maker.sh
+++ util/label_maker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo -n "" > bin/front_office.nes.ram.nl
-echo -n "" > bin/front_office.nes.0.nl
-echo -n "" > bin/front_office.nes.1.nl
+_OUTPUT_DIR=$2
+
+echo -n "" > $_OUTPUT_DIR/front_office.nes.ram.nl
+echo -n "" > $_OUTPUT_DIR/front_office.nes.0.nl
+echo -n "" > $_OUTPUT_DIR/front_office.nes.1.nl
 
 write_to_namelist() {
     echo "\$$1#$2#" >> $3
@@ -13,13 +15,13 @@ while IFS= read -r line; do
 
     case $_ADDRESS in
         0*)
-            write_to_namelist $_ADDRESS $_LABEL bin/front_office.nes.ram.nl
+            write_to_namelist $_ADDRESS $_LABEL $_OUTPUT_DIR/front_office.nes.ram.nl
         ;;
         [89AB]*)
-            write_to_namelist $_ADDRESS $_LABEL bin/front_office.nes.0.nl
+            write_to_namelist $_ADDRESS $_LABEL $_OUTPUT_DIR/front_office.nes.0.nl
         ;;
         [CDEF]*)
-            write_to_namelist $_ADDRESS $_LABEL bin/front_office.nes.1.nl
+            write_to_namelist $_ADDRESS $_LABEL $_OUTPUT_DIR/front_office.nes.1.nl
         ;;
     esac
 done < "$1"
