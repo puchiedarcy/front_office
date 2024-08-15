d3176c04c290a8733252cc6bb7e20b48c45d6ab9
Zachary Layman <puchiedarcy@users.noreply.github.com>
2024-08-15 12:34:28 -0500

    Report card.

---
+++ util/report_card_maker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo -n "" > $2
+
+declare -a tests
+declare -A error_codes
+for _RESULT in "$1/test_"*".results"; do
+    while IFS= read -r line; do
+        if [[ $line =~ ^.*_(.*)+\.prg.* ]]; then
+            _TEST=${BASH_REMATCH[1]}
+            tests+=("$_TEST")
+        fi
+
+        if [[ $line =~ ^PVExit.*([0-9]+).* ]]; then
+            error_codes["$_TEST"]=${BASH_REMATCH[1]}
+        fi
+    done < $_RESULT
+done
+
+_COUNTER=0
+_NUM_TESTS=${#tests[@]}
+(( _NUM_TESTS-- ))
+echo "{" >> $2
+for i in "${!tests[@]}"
+do
+    _TEST=${tests[$i]}
+    echo "\"$_TEST\": {" >> $2
+        echo "\"error_code\": \"${error_codes[$_TEST]}\"" >> $2
+    echo "}" >> $2
+    if [[ $_COUNTER < $_NUM_TESTS ]]; then
+        echo "," >> $2
+        (( _COUNTER++ ))
+    fi
+done
+echo "}" >> $2
+
+tr -d "\n" < $2 > /tmp/report_card.json
+mv /tmp/report_card.json $2
+echo "" >> $2
