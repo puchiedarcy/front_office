069003d171e156a92f46576f2eb5fd7919ac07cb
Zachary Layman <puchiedarcy@users.noreply.github.com>
2024-08-19 17:34:48 -0500

    Add cycles to report card.

--- util/report_card_maker.sh
+++ util/report_card_maker.sh
@@ -3,15 +3,22 @@ echo -n "" > $2
 
 declare -a tests
 declare -A error_codes
+declare -A cycles
+
 for _RESULT in "$1/test_"*".results"; do
     while IFS= read -r line; do
         if [[ $line =~ ^.*_(.*)+\.prg.* ]]; then
             _TEST=${BASH_REMATCH[1]}
             tests+=("$_TEST")
+            cycles["$_TEST"]=$_CYCLES
         fi
 
         if [[ $line =~ ^PVExit.*([0-9]+).* ]]; then
             error_codes["$_TEST"]=${BASH_REMATCH[1]}
+        fi
+
+        if [[ $line =~ ^([0-9]+)\ cycles ]]; then
+            _CYCLES=${BASH_REMATCH[1]}
         fi
     done < $_RESULT
 done
@@ -24,7 +31,8 @@ for i in "${!tests[@]}"
 do
     _TEST=${tests[$i]}
     echo "\"$_TEST\": {" >> $2
-        echo "\"error_code\": \"${error_codes[$_TEST]}\"" >> $2
+        echo "\"error_code\": \"${error_codes[$_TEST]}\"," >> $2
+        echo "\"cycles\": \"${cycles[$_TEST]}\"" >> $2
     echo "}" >> $2
     if [[ $_COUNTER < $_NUM_TESTS ]]; then
         echo "," >> $2
